40fd28ea5f59c3df944f852f63936b7c31e79014
Jason <jasondal91@gmail.com>
2024-10-14 22:37:58 +0900

    데이터베이스 접근 로직 추가

---
+++ src/test/java/com/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.board.repository;
+
+import com.board.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+}
